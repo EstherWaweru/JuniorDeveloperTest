1b43e9d45f7a09034998adc7d0c814edfb59a6f2
EstherWaweru <estherwaweru86@gmail.com>
2021-01-29 22:33:56 +0300

    Feature(Added Templates)
    What does this PR do?
    Creates base.html where other templates inherit from.Also created the sidebar template and the footer template.Created an empty dahboard template which is the same as the homepage.

--- crm/api/views.py
+++ crm/api/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def loginPage(request):
     return render(request,'login.html')
+def home(request):
+    return render(request,'dashboard.html')
