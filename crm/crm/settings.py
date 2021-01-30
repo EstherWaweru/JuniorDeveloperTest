2c8db3e3a9f9480ebf5d40f277665dfc0c8aafaa
EstherWaweru <estherwaweru86@gmail.com>
2021-01-30 18:51:06 +0300

    Hoping it works

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -28,7 +31,7 @@ SECRET_KEY = '83a26q&77bf%bib^q6t5!=5x=3yj33d6t(at6gtg$w-rm$i-a_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['djangocrmtest.herokuapp.com','djangotetcrm-cli.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['djangocrmtest.herokuapp.com','djangotetcrm-cli.herokuapp.com','127.0.0.1','tetcrm.herokuapp.com']
 
 
 # Application definition
@@ -82,16 +85,16 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
         # changing to mysql
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'crm',
-        'USER': 'root',
-        'PASSWORD': 'Kierah@98',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'crm',
+        # 'USER': 'root',
+        # 'PASSWORD': 'Kierah@98',
+        # 'HOST': 'localhost',
+        # 'PORT': '3306',
     }
 }
 
@@ -137,6 +140,7 @@ STATICFILES_DIRS=[
     STATIC_DIR,
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 #MEDIA
 MEDIA_ROOT=MEDIA_DIR
 MEDIA_URL='/media/'
