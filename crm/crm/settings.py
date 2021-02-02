aa0b273b95c09caa33f6bb364306815762f6f8e7
EstherWaweru <estherwaweru86@gmail.com>
2021-02-02 09:39:25 +0300

    Adding more deependencies to use django-heroku

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -154,3 +155,4 @@ DEFAULT_FROM_EMAIL="CRM<arrotechdesign@gmail.com>"
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+django_heroku.settings(locals())
